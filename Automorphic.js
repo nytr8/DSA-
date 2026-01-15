7e7df292b2a72f50e289ecab9a9f2971de4d9da0
Srutidev Barman <srutidevbarman2002@gmail.com>
2026-01-15 21:48:34 +0530

    added changes

---
+++ Automorphic.js
@@ -0,0 +1,18 @@
+function isAutomorphic(n) {
+  let numLength = String(n).length;
+  let sqrNum = n * n;
+  if (n == 0 || n == 1) {
+    return "Yes";
+  }
+  for (var i = 0; i < numLength; i++) {
+    let lastDigitOfEnteredNum = n % 10;
+    let lastDigitOfsqrNum = sqrNum % 10;
+    n = Math.floor(n / 10);
+    sqrNum = Math.floor(sqrNum / 10);
+    if (lastDigitOfsqrNum !== lastDigitOfEnteredNum) {
+      return "No";
+      break;
+    }
+  }
+  return "Yes";
+}
--- power calculator/app.js
+++
@@ -1,9 +0,0 @@
-function power(num, powr) {
-  let val = 1;
-  for (let i = 0; i < powr; i++) {
-    val = val * num;
-  }
-  return val;
-}
-let val = power(33, 2);
-console.log(val);
