d97ace30d848f0c509079b6f513ae3cb926fec3e
Srutidev Barman <srutidevbarman2002@gmail.com>
2026-01-23 18:06:21 +0530

    added code

---
+++ movezeroestoright.js
@@ -0,0 +1,12 @@
+function moveZerosToEnd(arr) {
+  for (var i = 0; i < arr.length - 1; i++) {
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
